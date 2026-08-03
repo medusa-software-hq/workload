876737112547465e4d1042851dd9ea64caad07f4
Jakub Trzebiatowski <jakub.trzebiatowski@medusa.software>
2026-08-03 21:21:17 +0200

    Enable Issues on the workload repo
    Flip has_issues to true so the repo can hold GitHub issues (a
    prerequisite for making it flow-friendly). Applied via .github/config
    Terraform after merge.

--- .github/config/github-repo.tf
+++ .github/config/github-repo.tf
@@ -11,7 +11,7 @@ resource "github_repository" "this" {
   is_template = false
 
   has_discussions = false
-  has_issues      = false
+  has_issues      = true
   has_projects    = false
   has_wiki        = false
 
